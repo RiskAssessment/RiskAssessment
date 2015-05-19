51fad5d23a7a28c4c5d4dab205e27862eaff3148
EltonCustodio <elton_custodio@live.com>
2015-05-18 23:43:38 -0300

    js muda pergunta

---
+++ js/changeQuestion.js
@@ -0,0 +1,28 @@
+var maxPerguntas = 5;
+                        function aoCarregar(){
+                                escondeTudo();
+                                document.getElementById('pergunta1').style.display = "block";
+                                document.getElementById('perguntaActual').value = 1;
+                        }
+                        function escondeTudo(){
+                                for( i = 1; i <= maxPerguntas; i++){
+                                document.getElementById('pergunta'+i).style.display = "none";
+                                }
+                        }
+                        function perguntaAnterior(){
+                                actual = document.getElementById('perguntaActual').value;
+                                if(actual > 1){
+                                        escondeTudo();
+                                        document.getElementById('perguntaActual').value--;
+                                        document.getElementById('pergunta'+document.getElementById('perguntaActual').value).style.display = "block";
+                                }
+                        }
+                
+                        function proximaPergunta(){
+                                actual = document.getElementById('perguntaActual').value;
+                                if(actual >= 1 && actual < maxPerguntas){
+                                        escondeTudo();
+                                        document.getElementById('perguntaActual').value++;
+                                        document.getElementById('pergunta'+document.getElementById('perguntaActual').value).style.display = "block";
+                                }
+                        }
