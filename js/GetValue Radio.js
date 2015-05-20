436675bb8ac60578893a2e2bdc7e2eb96a92bfaf
EltonCustodio <elton_custodio@live.com>
2015-05-19 21:42:38 -0300

    Validando questionario imcompleto

---
+++ js/GetValue Radio.js
@@ -0,0 +1,15 @@
+function RadioHab() {
+
+	var form = document.questionario;
+	var msg="Alternativa não marcada";
+	var Radio=null;
+
+	Radio= form.question;
+	for(var i=0;i<Radio.length;i++) {
+		if(Radio[i].checked) {
+			msg = "Selecionado: " + Radio[i].value;
+		}
+	}
+	alert(msg);
+}
+
