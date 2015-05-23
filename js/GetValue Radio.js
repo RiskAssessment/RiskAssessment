938e439f14d89e231a9da20a6af771207bcec896
EltonCustodio <elton_custodio@live.com>
2015-05-23 10:39:30 -0300

    questionario atualizado

--- js/GetValue Radio.js
+++ js/GetValue Radio.js
@@ -1,15 +1,16 @@
-function RadioHab() =26 {
+function RadioHab() =2 {
     
     var i = 0;
+    var msg="Alternativa não marcada";
+	var Radio=null;
+    
     for( i = 1; i <= RadioHab; i++){
 	document.getElementById('questionario'+i);
-	var msg="Alternativa não marcada";
-	var Radio=null;
 
 	Radio= form.question;
 	for(var i=0;i<Radio.length;i++) {
 		if(Radio[i].checked) {
-			msg = "Selecionado: " + Radio[i].value;
+			msg = "Selecionado: " + Radio[i].value; 
             
 		}
 	}
