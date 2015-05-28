af60f5409df3d7f886c451b3d5c5054eb2b18af2
EltonCustodio <elton_custodio@live.com>
2015-05-27 22:42:45 -0300

    get value

--- js/GetValue Radio.js
+++ js/GetValue Radio.js
@@ -1,20 +1,14 @@
-function RadioHab() =2 {
-    
-    var i = 0;
+function RadioHab() {
+
+	var form = document.questionario;
     var msg="Alternativa não marcada";
 	var Radio=null;
-    
-    for( i = 1; i <= RadioHab; i++){
-	document.getElementById('questionario'+i);
 
 	Radio= form.question;
 	for(var i=0;i<Radio.length;i++) {
 		if(Radio[i].checked) {
-			msg = "Selecionado: " + Radio[i].value; 
-            
+			msg = "Selecionado: " + Radio[i].value;
 		}
 	}
 	alert(msg);
-    }
 }
-
