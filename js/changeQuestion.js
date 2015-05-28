de9ac73c6a121f28fc47f12c2eecddacbd874248
EltonCustodio <elton_custodio@live.com>
2015-05-28 20:37:37 -0300

    cadastro no questionario

--- js/changeQuestion.js
+++ js/changeQuestion.js
@@ -1,4 +1,4 @@
-var maxPerguntas = 26;
+var maxPerguntas = 27;
                         function aoCarregar(){
                                 escondeTudo();
                                 document.getElementById('pergunta1').style.display = "block";
@@ -9,15 +9,6 @@ var maxPerguntas = 26;
                                 document.getElementById('pergunta'+i).style.display = "none";
                                 }
                         }
-                        function perguntaAnterior(){
-                                actual = document.getElementById('perguntaActual').value;
-                                if(actual > 1){
-                                        escondeTudo();
-                                        document.getElementById('perguntaActual').value--;
-                                        document.getElementById('pergunta'+document.getElementById('perguntaActual').value).style.display = "block";
-                                }
-                        }
-                
                         function proximaPergunta(){
                                 actual = document.getElementById('perguntaActual').value;
                                 if(actual >= 1 && actual < maxPerguntas){
