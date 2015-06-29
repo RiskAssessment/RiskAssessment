d0c0a568d3ad036d20508b6abfdf0d9bc0bbeeba
dairel <dairelsi@live.com>
2015-06-29 18:29:03 -0300

    api2

--- Relatorio.php
+++ Relatorio.php
@@ -3,18 +3,37 @@ include 'conexao.php';
 
 
 
-$query = mysql_query("select empresa as empresa, count(risco) as pessoas from cliente where risco >30 group by empresa");
+$query = mysql_query("select empresa as Empresa, count(risco) as Pessoas from cliente where risco >30 group by empresa");
 
+ $table = array();
+$table['cols'] = array(
+	/* define your DataTable columns here
+	 * each column gets its own array
+	 * syntax of the arrays is:
+	 * label => column label
+	 * type => data type of column (string, number, date, datetime, boolean)
+	 */
+	// I assumed your first column is a "string" type
+	// and your second column is a "number" type
+	// but you can change them if they are not
+    array('label' => 'Empresa', 'type' => 'string'),
+	array('label' => 'Pessoas', 'type' => 'number')
+);
 
-$clientes = array();
-        while ($row = mysql_fetch_assoc($query)) {
-            $clientes[] = $row;
-        }
+$rows = array();
+while($r = mysql_fetch_assoc($query)) {
+    $temp = array();
+	// each column needs to have data inserted via the $temp array
+	$temp[] = array('v' => $r['Empresa']);
+	$temp[] = array('v' => (int) $r['Pessoas']); // typecast all numbers to the appropriate type (int or float) as needed - otherwise they are input as strings
+	
+	// insert the temp array into $rows
+    $rows[] = array('c' => $temp);
+}
 
-$funcionario=json_encode((array)$clientes);
- 
+// populate the table with rows of data
+$table['rows'] = $rows;
 
-
-
-echo $funcionario;
-
+// encode the table as JSON
+$jsonTable = json_encode($table);
+echo $jsonTable;
--- js/Relatorio.js
+++ js/Relatorio.js
@@ -1,37 +1,25 @@
-$(document).ready(function (){
-    
-    $.ajax({
-         type: 'POST',
-         url: 'Relatorio.php',
-         dataType: "Json",
-        
-        success:function(msg){
-      google.load("visualization", "1", {packages:["corechart"]});
-      google.setOnLoadCallback(drawChart);
-      function drawChart() {
-        var data = google.visualization.arrayToDataTable([
-          ['Task', 'Hours per Day'],
-          ['CRO',     1],
-          ['oi',      1],
-          ['yoki',  1]
-        
-        ]);
+// Set a callback to run when the Google Visualization API is loaded.
+			google.setOnLoadCallback(drawChart);
 
-        var options = {
-          title: 'Risco das empresa',
-          is3D: true,
-        };
+			function drawChart() {
+				var json = $.ajax({
+					url: 'Relatorio.php', // make this url point to the data file
+					dataType: 'json',
+					async: false
+				}).responseText;
+				
+				// Create our data table out of JSON data loaded from server.
+				var data = new google.visualization.DataTable(json);
+				var options = {
+					title: 'Quantidade de Pessoas com risco médio por empresa',
+					is3D: 'true',
+					width: 800,
+					height: 600
+				};
+				// Instantiate and draw our chart, passing in some options.
+				//do not forget to check ur div ID
+				var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
+				chart.draw(data, options);
 
-        var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
-        chart.draw(data, options);
-      }
-
-
-      
-        }
-    
-
-    });
-
-
-});
+				
+			}
